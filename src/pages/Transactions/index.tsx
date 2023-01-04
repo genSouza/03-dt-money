899e8c39dc34fe79bdfd446e012cbcc631932e92
Genilton Souza <genilton.souza26@gmail.com>
2023-01-04 11:28:12 -0400

    feat(summary): add summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 };
